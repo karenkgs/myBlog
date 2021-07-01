3fad1884b0a46f72c9a9a00361724ffa6dfa91f7
Karen Garcia <karenkgs@gmail.com>
2021-07-01 20:49:50 +1000

    small fixes

--- src/controller/session.controller.ts
+++ src/controller/session.controller.ts
@@ -1,29 +1,24 @@
+import config from 'config';
 import { get } from 'lodash';
 import { Request, Response } from 'express';
-import { validatePassword } from '../service/user.service';
+import { loginUser } from '../service/user.service';
 import { createSession, createAccessToken, updateSession, findSessions } from '../service/session.service';
 import { sign } from '../utils/jwt.utils';
 
 export async function createUserSessionHandler(req: Request, res: Response) {
-    // validate the email and password
-    const user = await validatePassword(req.body);
+    const user = await loginUser(req.body);
 
     if (!user) {
         return res.status(401).send('Invalid username or password');
     }
 
-    // Create a session
     const session = await createSession(user._id, req.get('user-agent') || '');
-
-    // create access token
     const accessToken = createAccessToken({
         user,
         session,
     });
-
-    // create refresh token
     const refreshToken = sign(session, {
-        expiresIn: '1y', // config.get("refreshTokenTtl"), // 1 year
+        expiresIn: config.get('refreshTokenTtl'), // 1 year
     });
 
     // send refresh & access token back
@@ -40,7 +35,6 @@ export async function invalidateUserSessionHandler(req: Request, res: Response) 
 
 export async function getUserSessionsHandler(req: Request, res: Response) {
     const userId = get(req, 'user._id');
-
     const sessions = await findSessions({ user: userId, valid: true });
 
     return res.send(sessions);
--- src/service/user.service.ts
+++ src/service/user.service.ts
@@ -6,6 +6,7 @@ export async function createUser(input: DocumentDefinition<UserDocument>) {
     try {
         return await User.create(input);
     } catch (error) {
+        console.log('Error when creating user');
         throw new Error(error);
     }
 }
@@ -14,18 +15,18 @@ export async function findUser(query: FilterQuery<UserDocument>) {
     return User.findOne(query).lean();
 }
 
-export async function validatePassword({ email, password }: { email: UserDocument['email']; password: string }) {
+export async function loginUser({ email, password }: { email: UserDocument['email']; password: string }) {
     const user = await User.findOne({ email });
 
     if (!user) {
         return false;
     }
 
-    const isValid = await user.comparePassword(password);
+    const isLoginValid = await user.comparePassword(password);
 
-    if (!isValid) {
-        return false;
+    if (isLoginValid) {
+        return omit(user.toJSON(), 'password');
     }
 
-    return omit(user.toJSON(), 'password');
+    return false;
 }
